7ea72ed19fb5ef4aa6a5d54d217b8639ae13cb0f
yorlysoro <yorlysoro@gmail.com>
2022-06-16 20:18:53 -0400

    JavaScript API rest, post, get

--- js_api_rest/main.js
+++ js_api_rest/main.js
@@ -1,7 +1,7 @@
-console.log('Hello World');
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=9c1b676c-ad0e-40b6-9bd4-27d0b3101bf7"
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/images/favourites?limit=2&api_key=9c1b676c-ad0e-40b6-9bd4-27d0b3101bf7"
 
-const URL = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=9c1b676c-ad0e-40b6-9bd4-27d0b3101bf7"
-
+const spanError = document.getElementById('error');
 // let get_img = fetch(URL)
 // .then(response => response.json())
 // .then(data => {
@@ -13,15 +13,46 @@ const URL = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=9c1b676c
 
 // fetch with async / await
 async function get_img_async() {
-    const response = await fetch(URL);
+    const response = await fetch(API_URL_RANDOM);
     const data = await response.json();
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
-    console.log(data);
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    if (response.status !== 200) {
+        spanError.innerHTML = "Hubo un error " + response.status + " " + data.message;
+    }else{
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        console.log("Ramdon:", data);
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
+}
+
+async function get_fav_cat(){
+    const response = await fetch(API_URL_FAVORITES);
+    const data = await response.json();
+    if (response.status !== 200) {
+        spanError.innerHTML = "Hubo un error " + response.status + " " + data.message;
+    }
+    else{
+        console.log("Favourites:", data);
+    }
+}
+
+async function saveFavourites(){
+    const rest = await fetch(API_URL_FAVORITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            image_id: 12
+        }),
+    });
+    const data = await rest.json();
+    if (rest.status !== 200) {
+        spanError.innerHTML = "Hubo un error " + rest.status + " " + data.message;
+    }
+    console.log("Save:", data);
+
 }
 
 function get_cat(){
@@ -29,6 +60,6 @@ function get_cat(){
     btn.addEventListener('click', get_img_async);
 }
 get_img_async();
-
+get_fav_cat();
 //enpoint and query params
 
